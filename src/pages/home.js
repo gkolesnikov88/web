3ce1d538636d0c9f38315f58b851f0fd96e041da
George Kolesnikov <gkolesnikov88@gmail.com>
2023-09-25 06:27:38 +0300

    Added global wrapped Header and Navigation

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import Header from '../components/Header';
-import Navigation from '../components/Navigation';
 
 const Home = () => {
   return (
     <div>
-      <Header />
-      <Navigation />
       <h1>Notedly</h1>
       <p>This is the home page</p>
     </div>
