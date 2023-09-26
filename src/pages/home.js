e8cefa25c4a8d6dd67309e122191150a74efc657
George Kolesnikov <gkolesnikov88@gmail.com>
2023-09-26 07:02:51 +0300

    Added styled components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Button from '../components/Button';
 
 const Home = () => {
   return (
     <div>
       <h1>Notedly</h1>
       <p>This is the home page</p>
+      <Button>Click</Button>
     </div>
   );
 };
