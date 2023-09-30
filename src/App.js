d9086dee428026cb1c6e0c8ba53ae5b0bed6add5
George Kolesnikov <gkolesnikov88@gmail.com>
2023-09-30 07:37:53 +0300

    Added signup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, createHttpLink, gql, InMemoryCache } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 // import global styles
 import GlobalStyle from "./components/GlobalStyle";
@@ -9,12 +10,34 @@ import Pages from './pages';
 
 // configure our API URI & cache
 const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
 
+// check for a token and return the headers to the context
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
+
 const client = new ApolloClient({
-    uri,
+    link: authLink.concat(httpLink),
     cache,
-    connectToDevTools: true
+    resolvers: {},
+    connectToDevTools: true,
+});
+
+// check for a local token
+const data = {
+  isLoggedIn: !!localStorage.getItem('token')
+};
+// write the cache data on initial load
+cache.writeQuery({
+  query: gql`{ isLoggedIn @client }`,
+  data
 });
 
 const App = () => {
---
+++ src/pages/signup.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useMutation, useApolloClient, gql } from '@apollo/client';
+
+import Button from '../components/Button';
+
+const Wrapper = styled.div`
+  border: 1px solid #f5f4f0;
+  max-width: 500px;
+  padding: 1em;
+  margin: 0 auto;
+`;
+const Form = styled.form`
+  label,
+  input {
+    display: block;
+    line-height: 2em;
+  }
+  input {
+    width: 100%;
+    margin-bottom: 1em;
+  }
+`;
+
+const SIGNUP_USER = gql`
+  mutation signUp($email: String!, $username: String!, $password: String!) {
+    signUp(email: $email, username: $username, password: $password)
+  }
+`;
+
+const SignUp = props => {
+  // set the default state of the form
+  const [values, setValues] = useState();
+
+  // update the state when a user types in the form
+  const onChange = event => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  useEffect(() => {
+    document.title = 'Sign Up - Notedly';
+  });
+
+  // Apollo Client
+  const client = useApolloClient();
+
+  const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+    onCompleted: data => {
+      // store the JWT in localStorage
+      localStorage.setItem('token', data.signUp);
+      // update the local cache
+      client.writeData({data: { isLoggedIn: true }})
+      // redirect the user to the homepage
+      props.history.push('/');
+    }
+  });
+
+  return (
+    <Wrapper>
+      <h2>Sign Up</h2>
+      <Form
+        onSubmit={event => {
+          event.preventDefault();
+          signUp({
+            variables: {
+              ...values
+            }
+          });
+        }}
+      >
+        <label htmlFor="username">Username:</label>
+        <input
+          required
+          type="text"
+          id="username"
+          name="username"
+          placeholder="username"
+          onChange={onChange}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          required
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          onChange={onChange}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          required
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          onChange={onChange}
+        />
+        <button type="submit">Submit</button>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default SignUp;
